73bc27730a4c712c5a9a65a84d20a1ea376399db
jaideepghosh <er.jaideepghosh@gmail.com>
2020-08-16 03:57:16 +0530

    Updates

---
+++ precache-manifest.f82c483c7244532ec0918cbd5a7c3f64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e72b6d126512189105092c51c156813",
+    "url": "/ombd-react/index.html"
+  },
+  {
+    "revision": "e253d11e7d2bc8d6cabd",
+    "url": "/ombd-react/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "73a3ea4fccf4010cc7dc",
+    "url": "/ombd-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e253d11e7d2bc8d6cabd",
+    "url": "/ombd-react/static/js/2.b247e447.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ombd-react/static/js/2.b247e447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a3ea4fccf4010cc7dc",
+    "url": "/ombd-react/static/js/main.de433967.chunk.js"
+  },
+  {
+    "revision": "e84da2e846360c906c0b",
+    "url": "/ombd-react/static/js/runtime-main.b0c1a1fd.js"
+  }
+]);
